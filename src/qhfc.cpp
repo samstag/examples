e833f7d49f1e6665f6d28b81c55d08aafffe903d
Dave Knoester <dave.knoester@gmail.com>
2013-04-08 21:36:31 -0400

    qhfc example.

---
+++ src/qhfc.cpp
@@ -0,0 +1,9 @@
+//
+//  qhfc.cpp
+//  examples
+//
+//  Created by Dave Knoester on 4/8/13.
+//  Copyright (c) 2013 Dave Knoester. All rights reserved.
+//
+
+#include "qhfc.h"
---
+++ src/qhfc.h
@@ -0,0 +1,14 @@
+//
+//  qhfc.h
+//  examples
+//
+//  Created by Dave Knoester on 4/8/13.
+//  Copyright (c) 2013 Dave Knoester. All rights reserved.
+//
+
+#ifndef __examples__qhfc__
+#define __examples__qhfc__
+
+#include <iostream>
+
+#endif /* defined(__examples__qhfc__) */
